4842750d7fa79eaa3bc12f761a3b417c7fa34106
JMante1 <40006518+JMante1@users.noreply.github.com>
2021-09-29 19:47:46 -0600

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,14 +10,15 @@ def client():
 
 @pytest.fixture
 def eval_dict():
-    submit_dict = {"manifest": {"files": [
-                                    {"url": "http://synbiohub.org/expose/b41e63d6-10f4-4cac-b1c8-285f71156b56", "filename": "asdfasdf.xls", "type": "application/vnd.ms-excel"},
-                                    {"url": "http://synbiohub.org/expose/jkl9d8s7ufjqhoer8u709s", "filename": "file_name1.dna", "type": "application/xml"},
-                                    {"url": "http://synbiohub.org/expose/basdf-11230948f4-12344cac", "filename": "file_name2.xml", "type": "application/xml"},
-                                    {"url": "http://synbiohub.org/expose/09uj2k3j0", "filename": "file_name3.xml", "type": "application/xml"},
-                                    {"url": "http://synbiohub.org/expose/asdfasdf56", "filename": "file_name4.xml", "type": "application/xml"}
-                                    ]
-                                }
+    submit_dict = {"manifest":
+                   {"files": [
+                            {"url": "http://synbiohub.org/expose/b41e63d6-10f4-4cac-b1c8-285f71156b56", "filename": "asdfasdf.xls", "type": "application/vnd.ms-excel"},
+                            {"url": "http://synbiohub.org/expose/jkl9d8s7ufjqhoer8u709s", "filename": "file_name1.dna", "type": "application/xml"},
+                            {"url": "http://synbiohub.org/expose/basdf-11230948f4-12344cac", "filename": "file_name2.xml", "type": "application/xml"},
+                            {"url": "http://synbiohub.org/expose/09uj2k3j0", "filename": "file_name3.xml", "type": "application/xml"},
+                            {"url": "http://synbiohub.org/expose/asdfasdf56", "filename": "file_name4.xml", "type": "application/xml"}
+                    ]
+                    }
                    }
     submit_json = json.dumps(submit_dict)
     return submit_json
